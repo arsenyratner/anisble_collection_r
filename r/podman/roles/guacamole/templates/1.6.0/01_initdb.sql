f7852bc3778dd74fdcd3b1e7d73aee386f8242cb
arsenyratner <arsenyratner@gmail.com>
2026-01-13 21:36:44 +0300

    guacamole

--- r/podman/roles/guacamole/templates/1.6.0/01_initdb.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER '{{ pdmn_gcml_db_user }}'@'127.0.0.1' IDENTIFIED BY '{{ pdmn_gcml_db_pass }}';
-CREATE DATABASE {{ pdmn_gcml_db_name }};
-GRANT ALL PRIVILEGES ON {{ pdmn_gcml_db_name }}.* TO '{{ pdmn_gcml_db_user }}'@'127.0.0.1';
